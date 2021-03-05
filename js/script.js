126ccf369dd1001b111f848489cc57fa65afa78c
ianmanderson92 <ianmanderson92@users.noreply.github.com>
2021-03-05 00:37:09 -0700

    commit 2

--- js/script.js
+++ js/script.js
@@ -12,13 +12,17 @@ function updateCountryView() {
 
 function validateForm() {
 
+
+    //Check to see if DoB is valid
     var DoB = document.querySelector( "#txtDoB" );
-    var divDoBError = document.querySelector( "#divDoBError" )
+    var divDoBError = document.querySelector( "#divDoBError" );
+    var formIsValid = true;
 
     if (DoB.value == "") {
         divDoBError.classList.remove( "invisible" );
         divDoBError.innerHTML = "The Date of birth can not be empty."
         DoB.classList.add ( "hasError" );
+        formIsValid = false;
     }
     else {
         var DoBDate = new Date(DoB.value);
@@ -27,6 +31,7 @@ function validateForm() {
             divDoBError.classList.remove( "invisible" );
             divDoBError.innerHTML = "The Date of birth must be before today's date."
             DoB.classList.add ( "hasError" );
+            formIsValid = false;
         }
         else {
             divDoBError.classList.add( "invisible" );
@@ -34,7 +39,34 @@ function validateForm() {
             DoB.classList.remove ( "hasError" );
         }
     }
-    
 
-    return false;
+    //Check to see if country was added in part 3 if yes is selected
+    var ddCountry = document.querySelector( "#ddCountry" );
+    if ( ddCountry.value == "Yes" ) {
+        var txtCountry = document.querySelector( "#txtCountry" );
+        if ( txtCountry.value == "" ) {
+            document.querySelector( "#divCountryError" ).classList.remove( "invisible" );
+            txtCountry.classList.add( "hasError" );
+            formIsValid = false;
+        }
+        else {
+            document.querySelector( "#divCountryError" ).classList.add( "invisible" );
+            txtCountry.classList.remove( "hasError" );
+        }
+    }
+
+    //Check to make sure there are no illegal charachters in any input field
+    var elements = document.getElementsByTagName( " input" );
+    var invalidCharacters = ['#', '!', '~', '&', '<', '>', '`', '"']
+
+    for ( let i = 0; i < elements.length; i++ ) {
+        for ( let j = 0; j < invalidCharacters.length; j++ ) {
+            if ( elements[i].value.indexOf(invalidCharacters[j]) != -1 ) {
+                elements[i].classList.add( "hasError" );
+                formIsValid = false;
+            }
+        }
+    }
+    
+    return formIsValid;
 }
